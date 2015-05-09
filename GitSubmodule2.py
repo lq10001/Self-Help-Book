2d75bc3cc2e078da394ffb0c33170e5f7fd9620b
zhongwei <lq10001@gmail.com>
2015-05-09 11:13:04 +0800

    module

--- GitSubmodule2.py
+++ GitSubmodule2.py
@@ -4,6 +4,7 @@ import os, sys
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 def common_clear(submodule_directory, version):
+	print " ----- "  + submodule_directory
 	common_shell = ['cd '+ submodule_directory,
 	'git checkout ' + version,
 	'cd ..',
@@ -13,7 +14,8 @@ def common_clear(submodule_directory, version):
 	dr = submodule_directory
 	os.chdir(dr)
 	os.system("git checkout " + version)
-	os.chdir("..")
+	os.chdir(sys.path[0])
+	print " ======= "  + submodule_directory
 	os.system('git add '+submodule_directory)
 	os.system('git commit -m "moved ' + submodule_directory + ' to ' + version + '"')
 	os.system('git push')
@@ -25,7 +27,8 @@ if __name__ == '__main__':
 	["git submodule add --force https://git.oschina.net/dongfangx/SYS-JAVA-WEBSERVER-NUTZ.git src/main/java/com/ly/sys/", 'v1.0'],
 	["git submodule add --force https://git.oschina.net/dongfangx/COMMON-WEBSERVER-NUTZ-JAVA.git src/main/java/com/ly/comm/", 'v1.0'],
 	["git submodule add --force https://git.oschina.net/dongfangx/SYS-HTML-WEBSERVER-JAVA.git src/main/webapp/WEB-INF/sys/", 'v1.0'],
-	["git submodule add --force https://git.oschina.net/dongfangx/SYS-RESOURCES-WEBSERVER-NUTZ-JAVA.git src/main/webapp/sys/", 'v1.0']]
+	["git submodule add --force https://git.oschina.net/dongfangx/SYS-RESOURCES-WEBSERVER-NUTZ-JAVA.git src/main/webapp/sys/", 'v1.0'],
+	["git submodule add --force https://git.oschina.net/xknaan/B-JUI.git src/main/webapp/BJUI/", 'master']]
 
 	for cmd in commands:
 		p = subprocess.Popen(cmd[0], stdout=subprocess.PIPE, env=os.environ, shell=True)
